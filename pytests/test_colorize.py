5348184886df830e91b75668ce4bd8cf555ad081
Zac Swider <zac@concertobio.com>
2025-02-10 13:08:01 -0800

    performant colormapping with tests

---
+++ pytests/test_colorize.py
@@ -0,0 +1,103 @@
+import numpy as np
+import mergechannels as mc
+
+def test_create_rgb_from_arr():
+    x = np.ones((3,3), dtype=np.uint8)
+    rgb = mc.create_rgb_from_arr(x)
+    assert rgb.shape == (3,3,3)
+
+def test_apply_color_map():
+    x = np.ones((3,3), dtype=np.uint8)
+    rgb = mc.apply_color_map(x)
+    assert rgb.shape == (3,3,3)
+    assert rgb.dtype == np.uint8
+    assert np.allclose(
+        rgb,
+        np.array(
+            [[[0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2]],
+
+            [[0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2]],
+
+            [[0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2]]]
+        )
+    )
+    x = np.ones((3,3), dtype=np.uint8) * 255
+    rgb = mc.apply_color_map(x)
+    assert np.allclose(
+        rgb,
+        np.array(
+            [[[0, 188, 254],
+            [0, 188, 254],
+            [0, 188, 254]],
+
+            [[0, 188, 254],
+            [0, 188, 254],
+            [0, 188, 254]],
+
+            [[0, 188, 254],
+            [0, 188, 254],
+            [0, 188, 254]]]
+        )
+    )
+
+def test_apply_color_map_with_take():
+    x = np.ones((3,3), dtype=np.uint8)
+    rgb = mc.apply_color_map_with_take(x)
+    assert rgb.shape == (3,3,3)
+    assert rgb.dtype == np.uint8
+    assert np.allclose(
+        rgb,
+        np.array(
+            [[[0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2]],
+
+            [[0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2]],
+
+            [[0, 1, 2],
+            [0, 1, 2],
+            [0, 1, 2]]]
+        )
+    )
+    x = np.ones((3,3), dtype=np.uint8) * 255
+    rgb = mc.apply_color_map_with_take(x)
+    assert np.allclose(
+        rgb,
+        np.array(
+            [[[0, 188, 254],
+            [0, 188, 254],
+            [0, 188, 254]],
+
+            [[0, 188, 254],
+            [0, 188, 254],
+            [0, 188, 254]],
+
+            [[0, 188, 254],
+            [0, 188, 254],
+            [0, 188, 254]]]
+        )
+    )
+
+    # import matplotlib.pyplot as plt
+    # import time
+    # x = np.random.randint(0, 256, (30_000, 30_000), dtype=np.uint8)
+    # start = time.time()
+    # rgb = mc.apply_color_map(x)
+    # end = time.time()
+    # print(f"Time taken: {end - start} seconds")
+
+    # cmap = plt.get_cmap('viridis')
+    # start = time.time()
+    # rgb = cmap(x)
+    # end = time.time()
+    # print(f"Time taken: {end - start} seconds")
+    
+    
--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -1,3 +1,17 @@
-from .mergechannels import sum_as_string, mult, axpy  # type: ignore
+from .mergechannels import (
+    sum_as_string,
+    mult,
+    axpy,
+    create_rgb_from_arr,
+    apply_color_map,
+    apply_color_map_with_take,
+)  # type: ignore
 
-__all__ = ["sum_as_string", "mult", "axpy"]
+__all__ = [
+    "sum_as_string",
+    "mult",
+    "axpy",
+    "create_rgb_from_arr",
+    "apply_color_map",
+    "apply_color_map_with_take",
+]
