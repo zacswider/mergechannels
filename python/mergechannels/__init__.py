17e7d050fa8ce4226fc3d3213c76080ae777d226
Zac Swider <zac@concertobio.com>
2025-03-07 15:17:24 -0800

    remove unused python functions

--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -1,15 +1,9 @@
 from .mergechannels import (
-    sum_as_string,
-    mult,
-    axpy,
     create_rgb_from_arr,
     apply_color_map,
 )  # type: ignore
 
 __all__ = [
-    "sum_as_string",
-    "mult",
-    "axpy",
     "create_rgb_from_arr",
     "apply_color_map",
 ]
