f8b56fd21c0c1b5bd206204d3413767fbd58cddb
Zac Swider <zac@concertobio.com>
2025-03-26 19:42:01 -0400

    don't need to expose rgb from arr

--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -1,11 +1,9 @@
 from .mergechannels import (
-    create_rgb_from_arr,
     apply_color_map,
 )  # type: ignore
 from _internal import merge
 
 __all__ = [
-    "create_rgb_from_arr",
     "apply_color_map",
-	"merge",
+    "merge",
 ]
