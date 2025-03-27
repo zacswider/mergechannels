78e227a25e74e5c82afbbad0cf7b6758eb46c12b
Zac Swider <zac@concertobio.com>
2025-03-26 20:21:06 -0400

    type hints for internal py fxn

--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -1,7 +1,8 @@
-from .mergechannels import (
-    apply_color_map,
-)  # type: ignore
-from _internal import merge
+# Import Rust functions
+from .mergechannels import apply_color_map  # type: ignore
+
+# Import Python functions
+from ._internal import merge
 
 __all__ = [
     "apply_color_map",
--- python/mergechannels/__init__.pyi
+++ python/mergechannels/__init__.pyi
@@ -1,7 +1,16 @@
+from typing import Sequence, Literal
+
 import numpy as np
 
 from .luts import COLORMAPS
 
-def create_rgb_from_arr(x: np.ndarray) -> np.ndarray: ...
+
+def merge(
+	arrs: Sequence[np.ndarray],
+	colors: Sequence[COLORMAPS] = (),
+	blending: Literal['sum', 'max', 'min'] = 'max',
+) -> np.ndarray:
+    ...
 
 def apply_color_map(x: np.ndarray, cmap: COLORMAPS) -> np.ndarray: ...
+
