02bba69ad0d22276ecfec507855c08fec2562677
Zac Swider <zac@concertobio.com>
2025-05-04 22:20:45 -0700

    playing with multichannel dispatch

--- src/colorize.rs
+++ src/colorize.rs
@@ -1,27 +1,149 @@
 use crate::blend;
-use numpy::ndarray::{Array, Array3, ArrayView2};
+use numpy::ndarray::{Array, Array3, Array4, ArrayView2, ArrayView3};
 
 // Create a (y, x, 3) array with ones
-fn rgb_with_arr_shape(x: ArrayView2<u8>) -> Array3<u8> {
-    Array::ones((x.shape()[0], x.shape()[1], 3))
+fn img_to_rgb<T>(a: ArrayView2<T>) -> Array3<u8> {
+    Array::ones((a.shape()[0], a.shape()[1], 3))
 }
 
-pub fn colorize_single_channel_8bit(
-    arr:
-)
+// Create a (n, y, x, 3) array with ones, where n represents a time, z, or batch dimension
+fn stack_to_rgb<T>(a: ArrayView3<T>) -> Array4<u8> {
+    Array::ones((a.shape()[0], a.shape()[1], a.shape()[2], 3))
+}
 
-pub fn apply_color_map(arr: ArrayView2<u8>, cmap: &[[u8; 3]; 256]) -> Array3<u8> {
-    let mut rgb = rgb_with_arr_shape(arr);
+/*We expect either a single 2D array or an array of shape (n, y, x) where n represents either a
+* time, z, or batch. In either case, the color dimension should be added at the end.
+*/
+pub fn colorize_single_channel_8bit(
+    arr: ArrayView2<u8>,
+    low: f64,
+    high: f64,
+    cmap: &[[u8; 3]; 256],
+) -> Array3<u8> {
+    let mut rgb = img_to_rgb(arr);
+
+    let offset = low as f32;
+    let range = high - low;
+    let scale = if range.abs() > 1e-9 {
+        255.0 / range
+    } else {
+        0.0
+    };
+    let scale = scale as f32;
+
     let shape_y = arr.shape()[0];
     let shape_x = arr.shape()[1];
-
-    for i in 0..shape_y {
-        for j in 0..shape_x {
-            let idx = arr[[i, j]] as usize;
+    for y in 0..shape_y {
+        for x in 0..shape_x {
+            let value = arr[[y, x]];
+            let normalized_value = ((value as f32) - offset) * scale;
+            let idx = normalized_value.clamp(0.0, 255.0) as usize;
             let color = cmap[idx];
-            rgb[[i, j, 0]] = color[0];
-            rgb[[i, j, 1]] = color[1];
-            rgb[[i, j, 2]] = color[2];
+            rgb[[y, x, 0]] = color[0];
+            rgb[[y, x, 1]] = color[1];
+            rgb[[y, x, 2]] = color[2];
+        }
+    }
+    rgb
+}
+pub fn colorize_stack_8bit(
+    arr: ArrayView3<u8>,
+    low: f64,
+    high: f64,
+    cmap: &[[u8; 3]; 256],
+) -> Array4<u8> {
+    let mut rgb = stack_to_rgb(arr);
+
+    let offset = low as f32;
+    let range = high - low;
+    let scale = if range.abs() > 1e-9 {
+        255.0 / range
+    } else {
+        0.0
+    };
+    let scale = scale as f32;
+
+    let shape_n = arr.shape()[0];
+    let shape_y = arr.shape()[1];
+    let shape_x = arr.shape()[2];
+    for n in 0..shape_n {
+        for y in 0..shape_y {
+            for x in 0..shape_x {
+                let value = arr[[n, y, x]];
+                let normalized_value = ((value as f32) - offset) * scale;
+                let idx = normalized_value.clamp(0.0, 255.0) as usize;
+                let color = cmap[idx];
+                rgb[[n, y, x, 0]] = color[0];
+                rgb[[n, y, x, 1]] = color[1];
+                rgb[[n, y, x, 2]] = color[2];
+            }
+        }
+    }
+    rgb
+}
+pub fn colorize_single_channel_16bit(
+    arr: ArrayView2<u16>,
+    low: f64,
+    high: f64,
+    cmap: &[[u8; 3]; 256],
+) -> Array3<u8> {
+    let mut rgb = img_to_rgb(arr);
+
+    let offset = low as f32;
+    let range = high - low;
+    let scale = if range.abs() > 1e-9 {
+        255.0 / range
+    } else {
+        0.0
+    };
+    let scale = scale as f32;
+
+    let shape_y = arr.shape()[0];
+    let shape_x = arr.shape()[1];
+    for y in 0..shape_y {
+        for x in 0..shape_x {
+            let value = arr[[y, x]];
+            let normalized_value = ((value as f32) - offset) * scale;
+            let idx = normalized_value.clamp(0.0, 255.0) as usize;
+            let color = cmap[idx];
+            rgb[[y, x, 0]] = color[0];
+            rgb[[y, x, 1]] = color[1];
+            rgb[[y, x, 2]] = color[2];
+        }
+    }
+    rgb
+}
+pub fn colorize_stack_16bit(
+    arr: ArrayView3<u16>,
+    low: f64,
+    high: f64,
+    cmap: &[[u8; 3]; 256],
+) -> Array4<u8> {
+    let mut rgb = stack_to_rgb(arr);
+
+    let offset = low as f32;
+    let range = high - low;
+    let scale = if range.abs() > 1e-9 {
+        255.0 / range
+    } else {
+        0.0
+    };
+    let scale = scale as f32;
+
+    let shape_n = arr.shape()[0];
+    let shape_y = arr.shape()[1];
+    let shape_x = arr.shape()[2];
+    for n in 0..shape_n {
+        for y in 0..shape_y {
+            for x in 0..shape_x {
+                let value = arr[[n, y, x]];
+                let normalized_value = ((value as f32) - offset) * scale;
+                let idx = normalized_value.clamp(0.0, 255.0) as usize;
+                let color = cmap[idx];
+                rgb[[n, y, x, 0]] = color[0];
+                rgb[[n, y, x, 1]] = color[1];
+                rgb[[n, y, x, 2]] = color[2];
+            }
         }
     }
     rgb
@@ -35,7 +157,7 @@ pub fn apply_colors_and_merge(
     let first_arr = arrs[0]; // we guarantee that all arrays have the same shape before calling
     let shape_y = first_arr.shape()[0];
     let shape_x = first_arr.shape()[1];
-    let mut rgb = rgb_with_arr_shape(first_arr);
+    let mut rgb = img_to_rgb(first_arr);
     for i in 0..shape_y {
         for j in 0..shape_x {
             let mut px_vals: Vec<[u8; 3]> = Vec::with_capacity(arrs.len());
--- src/interface.rs
+++ src/interface.rs
@@ -1,56 +1,148 @@
 use crate::cmaps;
 use crate::colorize;
-use numpy::ndarray::ArrayView2;
-use numpy::npyffi::{npy_uint32, npy_uint8};
-use numpy::{PyArrayDyn, PyUntypedArray, PyUntypedArrayMethods, PyReadonlyArray, Element, PyReadonlyArrayDyn, IntoPyArray, PyArray3, PyReadonlyArray2, PyReadonlyArray3};
+use numpy::{
+    IntoPyArray, PyArrayDyn, PyReadonlyArray2, PyReadonlyArray3, PyUntypedArray,
+    PyUntypedArrayMethods,
+};
 use pyo3::prelude::*;
 use pyo3::types::PyAny;
 use pyo3::{Bound, Python};
 
 #[pyfunction]
 #[pyo3(name = "dispatch_single_channel")]
-pub fn dispatch_single_channel_py(
-    py: Python<'_>,
-    array_reference: &Bound<'_, PyAny>,
-    low: u8,
-    high: u8,
-    ndim: usize,
-    dtype: &str,
+pub fn dispatch_single_channel_py<'py>(
+    py: Python<'py>,
+    array_reference: &Bound<'py, PyAny>,
     cmap_name: &str,
-    // ) -> PyResult<(Bound<'py, PyArrayDyn<u8>)> {
-) -> PyResult<(())> {
-    match dtype {
+    low: f64,
+    high: f64,
+) -> PyResult<Bound<'py, PyArrayDyn<u8>>> {
+    let untyped_array = array_reference.downcast::<PyUntypedArray>()?;
+    let dtype = untyped_array.dtype().to_string();
+    let ndim = untyped_array.ndim();
+    match dtype.as_str() {
         "uint8" => {
             println!("doing uint8");
             match ndim {
                 2 => {
                     println!("doing 2D");
-                    let arr = array_reference.extract::<PyReadonlyArray2<u8>>()?;
-                },
+                    let py_arr = array_reference.extract::<PyReadonlyArray2<u8>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_single_channel_8bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
                 3 => {
                     println!("doing 3D");
-                    let arr = array_reference.extract::<PyReadonlyArray3<u8>>()?;
-                },
+                    let py_arr = array_reference.extract::<PyReadonlyArray3<u8>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_stack_8bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
                 _ => panic!("Recieved unsupported number of dimensions {:?}", ndim),
             }
-        },
+        }
         "uint16" => {
             println!("doing uint16");
             match ndim {
                 2 => {
                     println!("doing 2D");
-                    let arr = array_reference.extract::<PyReadonlyArray2<u16>>()?;
-                },
+                    let py_arr = array_reference.extract::<PyReadonlyArray2<u16>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_single_channel_16bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
                 3 => {
                     println!("doing 3D");
-                    let arr = array_reference.extract::<PyReadonlyArray3<u16>>()?;
-                },
+                    let py_arr = array_reference.extract::<PyReadonlyArray3<u16>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_stack_16bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
                 _ => panic!("Recieved unsupported number of dimensions {:?}", ndim),
             }
-        },
+        }
         _ => panic!("Received unsupported dtype: {:?}", dtype),
     }
-    Ok(())
+}
+
+#[pyfunction]
+#[pyo3(name = "dispatch_multi_channel")]
+pub fn dispatch_multi_channel_py<'py>(
+    py: Python<'py>,
+    array_references: &Bound<'py, PyAny>,
+    cmap_names: Vec<String>,
+    lows: Vec<f64>,
+    highs: Vec<f64>,
+) -> PyResult<Bound<'py, PyArrayDyn<u8>>> {
+    let cmaps: Vec<&[[u8; 3]; 256]> = cmap_names
+        .iter()
+        .map(|name| cmaps::load_cmap(name))
+        .collect();
+    if let Ok(arrs) = array_references
+        .try_iter()
+        .map(|arr_ref| arr_ref.extract::<PyReadonlyArray2<u8>>())
+        .into_iter()
+        .collect()
+    {
+        println!("Processing 2D u8 arrays")
+    } else {
+        println!("Error!")
+    }
+
+    let untyped_array = array_reference.downcast::<PyUntypedArray>()?;
+    let dtype = untyped_array.dtype().to_string();
+    let ndim = untyped_array.ndim();
+    match dtype.as_str() {
+        "uint8" => {
+            println!("doing uint8");
+            match ndim {
+                2 => {
+                    println!("doing 2D");
+                    let py_arr = array_reference.extract::<PyReadonlyArray2<u8>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_single_channel_8bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
+                3 => {
+                    println!("doing 3D");
+                    let py_arr = array_reference.extract::<PyReadonlyArray3<u8>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_stack_8bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
+                _ => panic!("Recieved unsupported number of dimensions {:?}", ndim),
+            }
+        }
+        "uint16" => {
+            println!("doing uint16");
+            match ndim {
+                2 => {
+                    println!("doing 2D");
+                    let py_arr = array_reference.extract::<PyReadonlyArray2<u16>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_single_channel_16bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
+                3 => {
+                    println!("doing 3D");
+                    let py_arr = array_reference.extract::<PyReadonlyArray3<u16>>()?;
+                    let arr = py_arr.as_array();
+                    let cmap = cmaps::load_cmap(cmap_name);
+                    let rgb = colorize::colorize_stack_16bit(arr, low, high, cmap);
+                    return Ok(rgb.into_dyn().into_pyarray(py));
+                }
+                _ => panic!("Recieved unsupported number of dimensions {:?}", ndim),
+            }
+        }
+        _ => panic!("Received unsupported dtype: {:?}", dtype),
+    }
 }
 
 // #[pyfunction]
