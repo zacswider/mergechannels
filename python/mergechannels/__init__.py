2121fcca70ed51abfba4c97b3715205b68b0e4ca
Zac Swider <zac@concertobio.com>
2025-05-13 21:22:26 -0700

    update stubs

--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -1,8 +1,10 @@
 # Import Rust functions
 from .mergechannels import (  # type: ignore
 	dispatch_single_channel,
+	dispatch_multi_channel,
 )
 
 __all__ = [
 	'dispatch_single_channel',
+	'dispatch_multi_channel',
 ]
--- python/mergechannels/__init__.pyi
+++ python/mergechannels/__init__.pyi
@@ -1,28 +1,30 @@
 
+from typing import Literal, Sequence
 import numpy as np
 
+from python.mergechannels._blending import BLENDING_OPTIONS
+from python.mergechannels._luts import COLORMAPS
 
-# def merge(
-#     arrs: Sequence[np.ndarray],
-#     colors: Sequence[COLORMAPS] = (),
-#     blending: Literal[BLENDING_OPTIONS] = 'max',
-#     saturation_limits: tuple[float, float] = (0.2, 99.8),
-# ) -> np.ndarray:
-#     ...
-#
-# def apply_color_map(
-#     cmap_name: COLORMAPS,
-#     saturation_limits: tuple[float, float] = (0.2, 99.8),
-# ) -> np.ndarray: ...
-#
-# def apply_colors_and_merge_nc(
-#     arrs: Sequence[np.ndarray],
-#     colors: Sequence[COLORMAPS] = (),
-#     blending: Literal[BLENDING_OPTIONS] = 'max',
-#     saturation_limits: Sequence[tuple[float, float]] = (),
-# ) -> np.ndarray: ...
+
+def merge(
+    arrs: np.ndarray | Sequence[np.ndarray],
+    colors: Sequence[COLORMAPS] = (),
+    blending: Literal[BLENDING_OPTIONS] = 'max',
+    saturation_limits: tuple[float, float] = (0.2, 99.8),
+) -> np.ndarray:
+    ...
 
 def dispatch_single_channel(
     arr: np.ndarray,
-) -> None:
+    cmap_name: str,
+    limits: tuple[float, float],
+) -> np.ndarray:
+    ...
+
+def dispatch_multi_channel(
+    arrs: Sequence[np.ndarray],
+    cmap_names: Sequence[str],
+    blending: Literal[BLENDING_OPTIONS],
+    limits: Sequence[tuple[float, float]],
+) -> np.ndarray:
     ...
