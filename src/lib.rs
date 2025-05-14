b5bc63da9ab1ff33b6d4a024c04fd79cfbb397d9
Zac Swider <zac@concertobio.com>
2025-05-13 21:00:31 -0700

    dispatch multichannel

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ mod cmaps;
 mod colorize;
 mod interface;
 
-use interface::dispatch_single_channel_py;
+use interface::{dispatch_multi_channel_py, dispatch_single_channel_py};
 use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn mergechannels(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(dispatch_single_channel_py, m)?)?;
-    // m.add_function(wrap_pyfunction!(apply_colors_and_merge_nc_py, m)?)?;
+    m.add_function(wrap_pyfunction!(dispatch_multi_channel_py, m)?)?;
     Ok(())
 }
