af72b2c05e207a12233481191ebdbaf50b701ab3
Zac Swider <zac@concertobio.com>
2025-05-15 22:36:54 -0700

    add merge fxn

--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -3,8 +3,11 @@ from .mergechannels import (  # type: ignore
 	dispatch_single_channel,
 	dispatch_multi_channel,
 )
+from ._internal import merge
+
 
 __all__ = [
 	'dispatch_single_channel',
 	'dispatch_multi_channel',
+	'merge',
 ]
