4ad9bdd57ea52968577abccceeaedc2702ff71e0
Zac Swider <zac.swider@gmail.com>
2025-12-30 15:01:22 -0800

    formatting, export get_cmap_array

--- python/mergechannels/__init__.py
+++ python/mergechannels/__init__.py
@@ -1,8 +1,8 @@
-# Import Rust functions
-from typing import Literal  # noqa: F401
-
-from ._internal import apply_color_map, merge
-from ._luts import COLORMAPS as _COLORMAPS
+from ._internal import (
+    apply_color_map,
+    get_cmap_array,
+    merge,
+)
 from .mergechannels import (  # type: ignore
     dispatch_multi_channel,
     dispatch_single_channel,
